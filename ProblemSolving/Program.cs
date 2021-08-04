0b3611d8ae997c34eeee80aa6a6efb37684fd358
mridulhossain <mridulfeni94@gmail.com>
2021-08-04 19:38:59 +0600

    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Reverse(321)); 
+        }
+
+
+        public static int Reverse(int x)
+        {
+
+
+            int remainder;
+            long reverse = 0;
+
+
+
+
+
+            if (x < 0)
+            {
+                var positive = x * -1;
+
+                while (positive > 0)
+                {
+                    remainder = positive % 10;
+                    reverse = reverse * 10 + remainder;
+                    positive /= 10;
+                }
+                reverse = reverse * -1;
+                bool test = reverse <= -2147483648;
+                if (test != true)
+                {
+                    return Convert.ToInt32(reverse);
+                }
+                else
+                {
+                    return 0;
+                }
+
+
+            }
+            else
+            {
+                while (x > 0)
+                {
+                    remainder = x % 10;
+                    reverse = reverse * 10 + remainder;
+                    x /= 10;
+                }
+
+                bool test = reverse <= 2147483647;
+                if (test != true)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Convert.ToInt32(reverse);
+                }
+
+            }
+
+        }
+    }
+}
